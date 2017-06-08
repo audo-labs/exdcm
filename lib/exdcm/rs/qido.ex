552ec35849e590931b4ad277456455cb65cb38df
Aeliton G. Silva <aeliton@gmail.com>
2017-06-08 17:52:06 -0300

    Add QIDO-RS series call

--- lib/exdcm/rs/qido.ex
+++ lib/exdcm/rs/qido.ex
@@ -1,6 +1,7 @@
 defmodule Exdcm.RS.QIDO do
 
   @base "http://localhost:8080/dcm4chee-arc"
+  @aet "DCM4CHEE"
   @moduledoc """
   Documentation for Exdcm.QIDO.RS.
   """
@@ -19,13 +20,19 @@ defmodule Exdcm.RS.QIDO do
   end
 
   def studies() do
-    HTTPoison.get!("#{@base}/aets/DCM4CHEE/rs/studies").body
+    HTTPoison.get!("#{@base}/aets/#{@aet}/rs/studies").body
     |> Poison.decode!
     |> Enum.map(&(Map.new(&1, fn{k, v} -> {Exdcm.Tag.name(k), Exdcm.VR.value(v)} end)))
   end
 
   def patients() do
-    HTTPoison.get!("#{@base}/aets/DCM4CHEE/rs/patients").body
+    HTTPoison.get!("#{@base}/aets/#{@aet}/rs/patients").body
+    |> Poison.decode!
+    |> Enum.map(&(Map.new(&1, fn{k, v} -> {Exdcm.Tag.name(k), Exdcm.VR.value(v)} end)))
+  end
+
+  def series(studyInstenceUid) do
+    HTTPoison.get!("#{@base}/aets/#{@aet}/rs/studies/#{studyInstenceUid}/series").body
     |> Poison.decode!
     |> Enum.map(&(Map.new(&1, fn{k, v} -> {Exdcm.Tag.name(k), Exdcm.VR.value(v)} end)))
   end
