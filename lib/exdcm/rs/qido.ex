63c174631f679fd5c8957a707dafc8adc1a3f370
aeliton <aeliton@ar.quei.ro>
2017-07-03 09:23:43 -0300

    Add studies() with studyInstanceUid arg

--- lib/exdcm/rs/qido.ex
+++ lib/exdcm/rs/qido.ex
@@ -6,21 +6,14 @@ defmodule Exdcm.RS.QIDO do
   Documentation for Exdcm.QIDO.RS.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Exdcm.QIDO.RS.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def studies() do
     HTTPoison.get!("#{@base}/aets/#{@aet}/rs/studies").body
+    |> Poison.decode!
+    |> Enum.map(&(Map.new(&1, fn{k, v} -> {Exdcm.Tag.name(k), Exdcm.VR.value(v)} end)))
+  end
+
+  def studies(studyInstanceUid) do
+    HTTPoison.get!("#{@base}/aets/#{@aet}/rs/studies?StudyInstanceUID=#{studyInstanceUid}").body
     |> Poison.decode!
     |> Enum.map(&(Map.new(&1, fn{k, v} -> {Exdcm.Tag.name(k), Exdcm.VR.value(v)} end)))
   end
