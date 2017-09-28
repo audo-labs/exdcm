1f48bb662a56f4dc5abf64555fd0d447aa70cc55
Aeliton G. Silva <aeliton@gmail.com>
2017-09-28 18:31:08 -0300

    Fix hackney_opts default arg for qido

--- lib/exdcm/rs/qido.ex
+++ lib/exdcm/rs/qido.ex
@@ -6,29 +6,29 @@ defmodule Exdcm.RS.QIDO do
   Documentation for Exdcm.QIDO.RS.
   """
 
-  def search_for_studies(base_url, query \\ %{}, response_params \\ %{}, hackney_opts \\ %{}) do
+  def search_for_studies(base_url, query \\ %{}, response_params \\ %{}, hackney_opts \\ []) do
     url = build_qido_url(base_url, "/studies", query, response_params)
     request(url, true, hackney_opts)
   end
 
-  def search_for_series(base_url, query \\ %{}, response_params \\ %{}, hackney_opts \\ %{}) do
+  def search_for_series(base_url, query \\ %{}, response_params \\ %{}, hackney_opts \\ []) do
     url = build_qido_url(base_url, "/series", query, response_params)
     request(url, true, hackney_opts)
   end
-  def search_for_series_by_study(base_url, study_instance_uid, query \\ %{}, response_params \\ %{}, hackney_opts \\ %{}) do
+  def search_for_series_by_study(base_url, study_instance_uid, query \\ %{}, response_params \\ %{}, hackney_opts \\ []) do
     url = build_qido_url(base_url, "/studies/#{study_instance_uid}/series", query, response_params)
     request(url, true, hackney_opts)
   end
 
-  def search_for_instances(base_url, query \\ %{}, response_params \\ %{}, hackney_opts \\ %{}) do
+  def search_for_instances(base_url, query \\ %{}, response_params \\ %{}, hackney_opts \\ []) do
     url = build_qido_url(base_url, "/instances", query, response_params)
     request(url, true, hackney_opts)
   end
-  def search_for_instances_by_study(base_url, study_instance_uid, query \\ %{}, response_params \\ %{}, hackney_opts \\ %{}) do
+  def search_for_instances_by_study(base_url, study_instance_uid, query \\ %{}, response_params \\ %{}, hackney_opts \\ []) do
     url = build_qido_url(base_url, "/studies/#{study_instance_uid}/instances", query, response_params)
     request(url, true, hackney_opts)
   end
-  def search_for_instances_by_series(base_url, study_instance_uid, series_instance_uid, query \\ %{}, response_params \\ %{}, hackney_opts \\ %{}) do
+  def search_for_instances_by_series(base_url, study_instance_uid, series_instance_uid, query \\ %{}, response_params \\ %{}, hackney_opts \\ []) do
     url = build_qido_url(base_url, "/studies/#{study_instance_uid}/series/#{series_instance_uid}/instances", query, response_params)
     request(url, true, hackney_opts)
   end
